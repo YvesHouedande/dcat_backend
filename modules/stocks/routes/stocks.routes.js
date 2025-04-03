e89c2830a96c0a22383d9dd29e7dfb70f40ff2c2
AxelOssoui <ossouiaxel@gmail.com>
2025-04-03 23:13:02 +0000

    mise en place des crud pour les tables marque-modele-entite

--- modules/stocks/routes/stocks.routes.js
+++ modules/stocks/routes/stocks.routes.js
@@ -4,9 +4,15 @@ const router = express.Router();
 // Import des sous-routes
 const familleRoutes = require("./famille.routes");
 const categorieRoutes = require("./categorie.routes");
+const marqueRoutes = require("./marque.routes");
+const modeleRoutes = require("./modele.route");
+const entiteRoutes = require("./entite.routes");
 
 // Montage des routes
 router.use("/familles", familleRoutes);
 router.use("/categories", categorieRoutes);
+router.use("/marques", marqueRoutes);
+router.use("/modeles", modeleRoutes);
+router.use("/entites", entiteRoutes);
 
 module.exports = router;
