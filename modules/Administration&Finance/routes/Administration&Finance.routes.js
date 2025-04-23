f2939a9fc778a6900d93e6bede09cd417c46972e
lucadete1 <adeteluc333@gmail.com>
2025-04-23 12:23:30 +0000

    erreur du chargemnt module administration et finance resolu

--- modules/Administration&Finance/routes/Administration&Finance.routes.js
+++ modules/Administration&Finance/routes/Administration&Finance.routes.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const entiteRoutes = require("./entity.route");
-const partnerRoutes = require("./partner.route");
-const demandRoutes = require("./demand.route");
+// const entiteRoutes = require("./entity.route");
+// const partnerRoutes = require("./partner.route");
+// const demandRoutes = require("./demand.route");
+const interlocuteurRoutes = require("../GestionAdministrative/routes/interlocuteur.route");
 // CRUD Routes
-router.use("/entites", entiteRoutes);
-router.use("/partners", partnerRoutes);
-router.use("/demandes", demandRoutes);
 
 module.exports = router;
