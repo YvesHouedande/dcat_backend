3dd19ced5ae56d83d3cb6dca2d30e4d7072f5e23
AxelOssoui <ossouiaxel@gmail.com>
2025-04-30 11:05:09 +0000

    finalisation des crud moyens de travail

--- modules/moyens_generaux/routes/moyens_generaux.routes.js
+++ modules/moyens_generaux/routes/moyens_generaux.routes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 // Import des sous-routes
 const outilsRoutes = require("./mouvementOutil.route");
+const moyenstravailRoutes = require("./moyensdeTravail.route"); //moyens de travail
 
 
 // Montage des routes
 router.use("/outils", outilsRoutes);
+router.use("/moyens-travail", moyenstravailRoutes);
 
 
 module.exports = router;
