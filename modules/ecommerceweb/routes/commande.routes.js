2a1e33697a4a2c4375790f0c474c539560472b33
Houedande <yveshouedandedocteur@gmail.com>
2025-06-26 14:07:29 +0000

    first commit[deploy]

--- modules/ecommerceweb/routes/commande.routes.js
+++ modules/ecommerceweb/routes/commande.routes.js
@@ -1,28 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-// const commandeController = require('../controllers/commande.controller');
-// const { verifyToken } = require('../middlewares/jwt.middleware');
-
-// router.post('/', verifyToken, commandeController.creerCommande);
-
-// router.get('/historique', 
-//     verifyToken, 
-//     commandeController.getHistoriqueCommandes
-//   );
-
-// router.get('/:id', 
-//   verifyToken, 
-//   commandeController.getOrderDetails
-// );
-
-// router.put('/:id/annuler', 
-//   verifyToken, 
-//   commandeController.cancelOrder
-// );
-
-// module.exports = router;
-
-
 const express = require('express');
 const router = express.Router();
 const commandeController = require('../controllers/commande.controller');
