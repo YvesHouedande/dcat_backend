d53cf5022e42c8237513fa77b989ea772e9b40b3
= <=>
2025-08-06 09:19:39 +0000

    soro V10.4.4 correction7 retouche getAllProducts With pagination and improve notification template for changing commande status [deploy]

--- server.js
+++ server.js
@@ -153,7 +153,7 @@ app.use((err, req, res, next) => {
 // DÉMARRAGE DU SERVEUR
 // =============================================
 // const PORT = process.env.PORT || 3000;
-const PORT = 3000;
+const PORT = 2000;
 server.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`);
   logger.info(`Keycloak configured for realm: ${keycloak.config.realm}`);
